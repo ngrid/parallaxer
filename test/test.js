8a97c1c54f0d90f7cc613c633c51cea0fef261c9
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2014-02-25 23:54:20 -0700

    Replaced the other stupid tests with tests based on selenium which rocks btw.

--- test/test.js
+++ test/test.js
@@ -1,19 +1,18 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var should = require('chai').should;
+var client = require('webdriverjs').remote({
+    desiredCapabilities: {
+        browserName: 'phantomjs'
+    }
+});
+
 describe('Test parallax effect on single background image.', function() {
-    var el = document.createElement("div");
-    el.id = "myDiv";
-    el.innerHTML = "Hello World!";
-    document.body.appendChild(el);
-    console.log(el);
-    console.log(document);
-    var myEl = document.getElementById('myDiv');
-
-    it("has the right text", function () {
-        assert.equal(myEl.innerHTML, "Hello World!");
+    before(function(done){
+        client.init().url('http://localhost:8080/test/testHarness.html', done);
     });
-
     it('background position should start off at 0, 0', function() {
     });
     it('background position should change after scrolling', function() {
     });
 });
-
