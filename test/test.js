97d4af147d4cd0e4cddbbd53d5a25b4324fdadc0
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2014-02-26 00:32:35 -0700

    Unit testing now happening on the title

--- test/test.js
+++ test/test.js
@@ -1,15 +1,22 @@
-var assert = require('chai').assert;
-var expect = require('chai').expect;
-var should = require('chai').should;
 var client = require('webdriverjs').remote({
     desiredCapabilities: {
-        browserName: 'phantomjs'
+        browserName: 'chrome'
     }
 });
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+//var should = require('chai').should;
 
 describe('Test parallax effect on single background image.', function() {
-    before(function(done){
-        client.init().url('http://localhost:8080/test/testHarness.html', done);
+    before(function(){
+        client.init().url('http://localhost:8080/test/testHarness.html');
+        client.scroll(0, 250);
+    });
+    it('make sure we got the right page back', function(done){
+        client.title(function(err, title){
+            expect(title.value).to.equal('Parallaxer Test\'s');
+            done();
+        });
     });
     it('background position should start off at 0, 0', function() {
     });
